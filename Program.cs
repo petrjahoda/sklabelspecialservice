245d1766ccac424356ca8d10a31ab09dc35bf922
Petr Jahoda <jahoda@zapsi.eu>
2020-02-17 10:22:24 +0100

    change: when closing idle, closing onl y idles that are running for more than 10 seconds.
    Took 3 minutes

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ using static System.Console;
 
 namespace sklabelspecialservice {
     internal static class Program {
-        private const string BuildDate = "2020.1.2.11";
+        private const string BuildDate = "2020.1.2.17";
         private const string DataFolder = "Logs";
         internal static string IpAddress;
         internal static string Port;
